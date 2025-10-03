df81088bb6b96e37b02128571cf9ef31aa99d3dc
HoangHung04 <Hunghvph46092@fpt.edu.vn>
2025-10-04 03:55:41 +0700

    cap nhap cpde

---
+++ resources/views/master.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html lang="en-US" prefix="og: http://ogp.me/ns#" class="no-js">
+
+
+<!-- Mirrored from mixtas.novaworks.net/home-v3/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 30 Sep 2025 12:52:17 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="profile" href="https://gmpg.org/xfn/11" />
+    <script>
+        document.documentElement.className = document.documentElement.className + ' yes-js js_active js'
+    </script>
+    <title>Friday &#8211; Fridaywear redesign - branding, website, mobile.</title>
+
+    <meta name='robots' content='max-image-preview:large' />
+    <style>
+        img:is([sizes="auto" i], [sizes^="auto," i]) {
+            contain-intrinsic-size: 3000px 1500px
+        }
+    </style>
+    <!-- Novaworks Meta Tags -->
+    <meta property="description"
+        content="Urban Muse City Chic with a Feminine Twist Shop now Travel bags Shop now Urban Muse Classic Beauties for Modern Women Shop now You are innew arrivalsbest sellertrending Add to wishlist Browse wishlist Add to cart Hoodies Dime Classic Remastered Hoodie, black &#036;140.00 Add to wishlist Browse wishlist Select options Men Dime Sunny Tee, forest green &#036;55.00 Add to wishlist…" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:title" content="Home v3 - Mixtas" />
+    <meta property="og:description"
+        content="Urban Muse City Chic with a Feminine Twist Shop now Travel bags Shop now Urban Muse Classic Beauties for Modern Women Shop now You are innew arrivalsbest sellertrending Add to wishlist Browse wishlist Add to cart Hoodies Dime Classic Remastered Hoodie, black &#036;140.00 Add to wishlist Browse wishlist Select options Men Dime Sunny Tee, forest green &#036;55.00 Add to wishlist…" />
+    <meta property="og:image" content="https://mixtas.b-cdn.net/wp-content/themes/mixtas/assets/images/logo.svg" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://mixtas.novaworks.net/home-v3/" />
+    <meta property="og:site_name" content="Mixtas" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:description"
+        content="Urban Muse City Chic with a Feminine Twist Shop now Travel bags Shop now Urban Muse Classic Beauties for Modern Women Shop now You are innew arrivalsbest sellertrending Add to wishlist Browse wishlist Add to cart Hoodies Dime Classic Remastered Hoodie, black &#036;140.00 Add to wishlist Browse wishlist Select options Men Dime Sunny Tee, forest green &#036;55.00 Add to wishlist…" />
+    <meta name="twitter:title" content="Home v3 - Mixtas" />
+    <meta name="twitter:image" content="https://mixtas.b-cdn.net/wp-content/themes/mixtas/assets/images/logo.svg" />
+    <!-- End Novaworks Meta Tags -->
+
+    @include('layouts.css')
+</head>
+
+<body
+    class="wp-singular page-template page-template-elementor_header_footer page page-id-917 wp-embed-responsive wp-theme-mixtas ltr theme-mixtas woocommerce-no-js woo-variation-swatches wvs-behavior-blur wvs-theme-mixtas wvs-show-label wvs-tooltip elementor-default elementor-template-full-width elementor-kit-6 elementor-page elementor-page-917 blog-sidebar-active blog-sidebar-right single-blog-sidebar-active  kitify--enabled">
+	@include('home.index')
+    <!-- .kitify-site-wrapper -->
+@include('layouts.js')
+        src="../../mixtas.b-cdn.net/wp-content/plugins/kitify/assets/js/addons/tabs35ee.js?ver=1759215510"
+        id="kitify-w__tabs-js"></script>
+</body>
+
+<!-- Mirrored from mixtas.novaworks.net/home-v3/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 30 Sep 2025 12:52:44 GMT -->
+
+</html>
+
+
+<!-- Page cached by LiteSpeed Cache 6.5.2 on 2025-09-30 06:58:30 -->
