bea373cbea35851e031ca5043893a7d8f6e4dd01
dung <dungndph52740@gmail.com>
2025-10-09 15:35:58 +0700

    Đăng ký

--- routes/web.php
+++ routes/web.php
@@ -66,15 +66,23 @@ Route::prefix('account')->group(function () {
 |--------------------------------------------------------------------------
 */
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-Route::get('/forgot-password', [AuthController::class, 'showForgotForm'])->name('forgot-password');
-Route::post('/forgot-password', [AuthController::class, 'sendResetLink'])->name('forgot-password.email');
-Route::get('/reset-password/{token}', [AuthController::class, 'showResetForm'])->name('forgot-password.reset');
-Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('forgot-password.update');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login'); 
+Route::post('/login', [AuthController::class, 'login']); 
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register'); 
+Route::post('/register', [AuthController::class, 'register']); 
+Route::get('/forgot-password', [AuthController::class, 'showForgotForm'])->name('forgot-password'); 
+Route::post('/forgot-password', [AuthController::class, 'sendResetLink'])->name('forgot-password.email'); 
+Route::get('/reset-password/{token}', [AuthController::class, 'showResetForm'])->name('forgot-password.reset'); 
+Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('forgot-password.update'); 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+
+// ==========================
+// GOOGLE LOGIN ROUTES
+// ==========================
+Route::get('/auth/google', [AuthController::class, 'redirectToGoogle'])->name('auth.google');
+Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback'])->name('auth.google.callback');
 
 
 /*
