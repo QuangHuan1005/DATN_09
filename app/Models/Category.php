a17b7a00f92dd0bff902a74937a7057e392ec014
QuangHuan1005 <quanghuanph52249@gmail.com>
2025-10-10 22:01:58 +0700

    Lưu thay đổi cho Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,18 +8,6 @@ use Illuminate\Database\Eloquent\SoftDeletes; // ✅ thêm dòng này
 
 class Category extends Model
 {
-Huy
-    use HasFactory;
-
-    protected $table = 'categories';
-
-    protected $fillable = ['parent_id', 'name', 'slug', 'description'];
-
-    public function products()
-    {
-        return $this->hasMany(Product::class, 'category_id');
-    }
-
     use HasFactory, SoftDeletes; // ✅ thêm SoftDeletes vào đây
 
     protected $fillable = [
@@ -28,5 +16,4 @@ Huy
         'description',
         'parent_id',
     ];
-main
 }
