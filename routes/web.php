6b86bcf52d4904d8e1755feb27bc414a5ee22abe
HoangKenBi <HoangTHTH2202002@fpt.edu.vn>
2025-10-12 16:58:52 +0700

    Sửa lỗi route

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::prefix('products')->group(function () {
     Route::get('/category/{slug}', [ProductController::class, 'showByCategory'])->name('products.category');
     Route::get('/color/{slug}', [ProductController::class, 'showByColor'])->name('products.color');
     Route::get('/size/{slug}', [ProductController::class, 'showBySize'])->name('products.size');
-
+});
 // Danh mục sản phẩm
 Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('categories.show');
 
@@ -49,7 +49,7 @@ Route::prefix('cart')->group(function () {
 // // Thanh toán
 // Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
 // Route::post('/checkout/process', [CheckoutController::class, 'process'])->name('checkout.process');
-  
+
 // Đơn hàng người dùng
 Route::prefix('orders')->group(function () {
     Route::get('/', [OrderController::class, 'index'])->name('orders.index');
@@ -136,3 +136,4 @@ Route::prefix('admin')
         // // Người dùng
         // Route::resource('users', AdminUserController::class);
     });
+
